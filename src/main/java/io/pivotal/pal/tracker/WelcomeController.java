9168f1ff85959c098633ccc94d1ed0a4c692cd5b
parkyang <parkkr@lgcns.com>
2019-09-10 09:51:25 +0900

    pal-tracker second

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    private String hello;
+
+    public WelcomeController(@Value("${welcome.message}") String s) {
+        this.hello = s;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+
+        return hello;
     }
 }
