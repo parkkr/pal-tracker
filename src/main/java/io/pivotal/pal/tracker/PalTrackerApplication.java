6b0e817bb8a43e2b6d82330b785513fdda6842de
parkyang <parkkr@lgcns.com>
2019-09-11 14:20:35 +0900

    JdbcTemplate

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
     @Bean
-    public TimeEntryRepository timeEntryRepository()
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource)
     {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -7,9 +7,15 @@ public interface TimeEntryRepository {
 
     public TimeEntry find(long timeEntryId);
 
+    //TimeEntry find(Long id);
+
     public List<TimeEntry> list() ;
 
     public TimeEntry update(long eq, TimeEntry any) ;
 
     public void delete(long timeEntryId) ;
+
+    //TimeEntry update(Long id, TimeEntry timeEntry);
+
+    //void delete(Long id);
 }
